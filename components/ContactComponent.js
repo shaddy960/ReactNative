ee3502661a4d4ba3f391e8c7c394cbd5ed52d7f9
shaddyHome <raisan1694@gmail.com>
2020-04-26 21:20:06 +0530

    Social

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { Text, View, ScrollView } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card, Button, Icon } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer'
 
-function RenderAddress() {
+function RenderAddress(props) {
 
     return (
         <ScrollView>
@@ -12,6 +13,12 @@ function RenderAddress() {
                     <Text style={{ margin: 10 }}>
                         {`121, Clear Water Bay Road\n\nClear Water Bay, Kowloon\n\nHONG KONG\n\nTel: +852 1234 5678\n\nFax: +852 8765 4321\n\nEmail:confusion@food.net`}
                     </Text>
+                    <Button
+                        title="Send Email"
+                        buttonStyle={{backgroundColor: "#512DA8"}}
+                        icon={<Icon name='envelope-o' type='font-awesome' color='white' />}
+                        onPress={props.mails}
+                        />
                 </Card>
             </Animatable.View>
         </ScrollView>
@@ -21,13 +28,21 @@ function RenderAddress() {
 
 class Contact extends Component {
 
+    sendMail() {
+        MailComposer.composeAsync({
+            recipients: ['confusion@food.net'],
+            subject: 'Enquiry',
+            body: 'To whom it may concern:'
+        })
+    }
+
     static navigationOptions = {
         title: 'Contact Us'
     };
 
     render() {
         return (
-            <RenderAddress />
+            <RenderAddress mails={() => this.sendMail()} />
         );
     }
 }
--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, Alert, PanResponder } from 'react-native';
+import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
 import { connect } from 'react-redux';
@@ -37,6 +37,16 @@ function RenderDish(props) {
             return true;
         else
             return false;
+    }
+
+    const shareDish = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: title + ': ' + message + ' ' + url,
+            url: url
+        },{
+            dialogTitle: 'Share ' + title
+        })
     }
 
     const panResponder = PanResponder.create({
@@ -94,6 +104,14 @@ function RenderDish(props) {
                             color='#0000FF'
                             onPress={() => props.onPress1()}
                         />
+                         <Icon
+                            raised
+                            reverse
+                            name='share'
+                            type='font-awesome'
+                            color='#51D2A8'
+                            style={styles.cardItem}
+                            onPress={() => shareDish(dish.name, dish.description, baseUrl + dish.image)} />
                     </View>
                 </Card>
             </Animatable.View>
